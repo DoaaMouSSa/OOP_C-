7b865f4704148609f4a69bc92b98aed1e00ec784
DoaaMouSSa <doaamoussa5@gmail.com>
2023-06-09 17:13:44 +0300

    #Practise-Basic

---
+++ oop_op_overloading/Models/Rectangle.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_op_overloading
+{
+   public class Rectangle
+    {
+        double height;
+        double width;
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(int h,int w)
+        {
+            this.height = h;
+            this.width = w;
+        }
+       public void printDimensions()
+        {
+            Console.WriteLine("height = " + this.height + " , width = " + this.width);
+        }
+        public static void isEqual(Rectangle r1,Rectangle r2)
+        {
+            if (r1 == r2) Console.WriteLine("equality");
+            else if(r1 != r2) Console.WriteLine("NOT equality");
+            else Console.WriteLine("Error");
+        }
+        public static void whatsGreater(Rectangle r1, Rectangle r2)
+        {
+            if (r1 >= r2) Console.WriteLine("r1 is Greater");
+            else if (r1 <= r2) Console.WriteLine("r2 is Grater");
+            else Console.WriteLine("Error");
+        }
+        /*****************arithmetc operation*************/
+        public static Rectangle operator +(Rectangle r1,Rectangle r2)
+        {
+            Rectangle r = new Rectangle();
+            r.height = r1.height + r2.height;
+            r.width = r1.width + r2.width;
+            return r;
+        }
+        public static Rectangle operator -(Rectangle r1, Rectangle r2)
+        {
+            Rectangle r = new Rectangle();
+            r.height = r1.height - r2.height;
+            r.width = r1.width - r2.width;
+            return r;
+        }
+        public static Rectangle operator *(Rectangle r1, Rectangle r2)
+        {
+            Rectangle r = new Rectangle();
+            r.height = r1.height * r2.height;
+            r.width = r1.width * r2.width;
+            return r;
+        }
+        public static Rectangle operator /(Rectangle r1, Rectangle r2)
+        {
+            Rectangle r = new Rectangle();
+            r.height = r1.height / r2.height;
+            r.width = r1.width / r2.width;
+            return r;
+        }
+        public static Rectangle operator %(Rectangle r1, Rectangle r2)
+        {
+            Rectangle r = new Rectangle();
+            r.height = r1.height % r2.height;
+            r.width = r1.width % r2.width;
+            return r;
+        }
+
+        /*****************unary increment*************/
+
+        public static Rectangle operator ++(Rectangle r)
+        {
+            r.height++;
+            r.width++;
+            return r;
+        }
+
+        /*****************unary decrement*************/
+
+        public static Rectangle operator --(Rectangle r)
+        {
+            r.height--;
+            r.width--;
+            return r;
+        }
+
+        /*****************relational operations*************/
+        public static bool operator ==(Rectangle r1,Rectangle r2)
+        {
+            bool isEqual = false;
+            if(r1.height==r2.height && r1.width == r2.width)
+            {
+                isEqual = true;
+            }
+            return isEqual;
+        }
+        public static bool operator !=(Rectangle r1, Rectangle r2)
+        {
+            bool isEqual = false;
+            if (r1.height != r2.height || r1.width != r2.width)
+            {
+                isEqual = true;
+            }
+            return isEqual;
+        }
+        public static bool operator >=(Rectangle r1, Rectangle r2)
+        {
+            bool isGreater = false;
+            if (r1.height >= r2.height && r1.width >= r2.width)
+            {
+                isGreater = true;
+            }
+            return isGreater;
+        }
+        public static bool operator <=(Rectangle r1, Rectangle r2)
+        {
+            bool isSmaller = false;
+            if (r1.height <= r2.height && r1.width <= r2.width)
+            {
+                isSmaller = true;
+            }
+            return isSmaller;
+        }
+        /*****************implicit cast*************/
+
+        public static implicit operator Rectangle(int[] x)
+        {
+            Rectangle r = new Rectangle();
+            r.height = x[0];
+            r.width = x[1];
+            return r;
+        }
+        /*****************explicit cast*************/
+
+        public static explicit operator double[](Rectangle r)
+        {
+            double[] x=new double[2]; 
+             x[0]= r.height;
+            x[1]= r.width;
+            return x;
+        }
+    }
+}
--- oop_op_overloading/Program.cs
+++ oop_op_overloading/Program.cs
@@ -6,7 +6,46 @@ namespace oop_op_overloading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle r1 = new Rectangle(10,11);
+            Rectangle r2 = new Rectangle(6, 6);
+            Rectangle r3 = new Rectangle();
+            Rectangle r4 = new Rectangle(100,101);
+            r3 = r1 + r2;   //(16,17)
+            r3.printDimensions();
+
+            r3 = r1 - r2;   //(4,5)
+            r3.printDimensions();
+
+            r3 = r1 * r2;   //(60,66)
+            r3.printDimensions();
+
+            r3 = r1 / r2;   //(1,1)
+            r3.printDimensions();
+
+            r3 = r1 % r2;   //(4,5)
+            r3.printDimensions();
+
+            r4++;             //(101,102)
+            r4.printDimensions();
+
+            r4--;
+            r4--;          //(99,100)
+            r4.printDimensions();
+            Rectangle.isEqual(r1, r2);
+            Rectangle.whatsGreater(r1, r2);
+
+            /*****************implicit cast*************/
+            int[] y = { 200,300 };
+            Rectangle r_imp = new Rectangle();
+            r_imp = y;
+            r_imp.printDimensions();
+
+            /*****************explicit cast*************/
+            double[] z =new double[2] ;
+            Rectangle r_exp = new Rectangle(500,600);
+            z = (double[])r_exp;
+            Console.WriteLine(z[0]+ ",,,"+ z[1])
+;
         }
     }
 }
